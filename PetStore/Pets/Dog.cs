9da58b11b8eb6d36c8673773472292bacc6a6dde
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-07-20 18:18:53 -0500

    Add project files.

---
+++ PetStore/Pets/Dog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Pets
+{
+    class Dog //implicitly internal
+    {
+        
+        // Access Modifiers:
+        // private: only things in this class can use it --DEFAULT MODIFIER--IMPLICIT
+        // public: everything that can see the class can access it
+        // internal: anything in this project can access it
+        // protected: anything that inherits from me can access
+
+        // Properties (usually found towards top of the class)
+
+        // public is an accessibility modifier
+        // next part of a property is the type
+        // then the name of the property
+        public string Size { get; set; } //auto-property
+        public short Weight { get; private set; }
+        public string Name { get; set; }
+
+        // Constructor, constructor method
+        public Dog(string name, short weight, string size)
+        {
+            Name = name;
+            Size = size;
+            Weight = weight;
+        }
+
+        // Method Signature
+        // Access Modifier - return type - name - args
+        // Void means not returning anything
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks noisily.");
+        }
+        // params are camelCase
+        public void Eat(string typeOfFood)
+        {
+           
+            if (typeOfFood.ToLower() == "healthy food")
+            {
+                Weight -= 1;
+            }
+            Console.WriteLine($"{Name} ate {typeOfFood}. {Name} weighs {Weight}");
+        }
+    }
+}
---
+++ PetStore/Program.cs
@@ -0,0 +1,31 @@
+﻿using PetStore.Pets;
+using System;
+
+
+namespace PetStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your dogs name");
+            var userNameInput = Console.ReadLine();
+            Console.WriteLine($"Enter {userNameInput}'s size");
+            var userSizeInput = Console.ReadLine();
+            Console.WriteLine($"Enter {userNameInput}'s weight");
+            short userWeightInput = short.Parse(Console.ReadLine());
+
+            var dog1= new Dog(userNameInput, userWeightInput, userSizeInput);
+            dog1.Bark();
+            dog1.Eat("healthy food");
+
+            var daisy = new Dog("Daisy", 50, "Medium");
+            daisy.Name = "Daisy";
+            daisy.Bark();
+            daisy.Eat("unhealthy food");
+
+            // this won't work, can't dynamically add properties to a class in c#
+            //melba.CoatType = "short";
+        }
+    }   
+}
